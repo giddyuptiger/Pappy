08980df10bbc1dab27279a8cb071ea4c073cac93
giddyuptiger <jeremydirons@gmail.com>
2021-02-24 20:24:47 -0800

    PICK, PACK, orders by status. working. few bugs

--- Pappy.js
+++ Pappy.js
@@ -59,7 +59,7 @@ function myFunction() {
         ck +
         "&consumer_secret=" +
         cs +
-        "&per_page=10" +
+        "&per_page=100" +
         "&page=" +
         (i + 1);
       // Logger.log(url);
@@ -104,8 +104,12 @@ function myFunction() {
     var doc = SpreadsheetApp.getActiveSpreadsheet();
 
     var temp = doc.getSheetByName("ORDERS - processing");
+    let otherSheet = doc.getSheetByName("ORDERS - other");
+    let completedSheet = doc.getSheetByName("ORDERS - completed");
 
     var ordersArray = [];
+    var ordersCompletedArray = [];
+    var ordersOtherArray = [];
 
     var headers = [
       "First",
@@ -129,46 +133,50 @@ function myFunction() {
       "Status",
       "Order Key",
     ];
+
     ordersArray.push(headers);
+    ordersCompletedArray.push(headers);
+    ordersOtherArray.push(headers);
 
     var consumption = {};
     let pickList = [];
+    let packList = [];
+    // let customerList = [];
 
     var arrayLength = params.length;
     // Logger.log(arrayLength);
     for (var i = 0; i < arrayLength; i++) {
-      if (params[i]["status"] !== "processing") {
-        continue;
-      }
       var a, c, d;
+      // let = orderObj
       var container = [];
 
       a = container.push(params[i]["billing"]["first_name"]);
-      // Logger.log(a);
 
       a = container.push(params[i]["billing"]["last_name"]);
 
-      a = container.push(
+      let billingAddress =
         params[i]["billing"]["address_1"] +
-          " " +
-          params[i]["billing"]["postcode"] +
-          " " +
-          params[i]["billing"]["city"]
-      );
+        " " +
+        params[i]["billing"]["postcode"] +
+        " " +
+        params[i]["billing"]["city"];
 
-      a = container.push(
-        params[i]["shipping"]["first_name"] +
-          " " +
-          params[i]["shipping"]["last_name"] +
-          " " +
-          params[i]["shipping"]["address_1"] +
-          " " +
-          params[i]["shipping"]["postcode"] +
-          " " +
-          params[i]["shipping"]["city"] +
-          " " +
-          params[i]["shipping"]["country"]
-      );
+      a = container.push(billingAddress);
+
+      let shippingAddress =
+        // params[i]["shipping"]["first_name"] +
+        // " " +
+        // params[i]["shipping"]["last_name"] +
+        // " " +
+        params[i]["shipping"]["address_1"] +
+        " " +
+        params[i]["shipping"]["postcode"] +
+        " " +
+        params[i]["shipping"]["city"] +
+        " " +
+        params[i]["shipping"]["country"];
+
+      a = container.push(shippingAddress);
 
       a = container.push(params[i]["billing"]["phone"]);
 
@@ -196,7 +204,7 @@ function myFunction() {
         total_line_items_quantity += qty;
 
         let itemObj = { name: item, qty: qty };
-        Logger.log(itemObj);
+        // Logger.log(itemObj);
 
         let found = false;
         for (let entry of pickList) {
@@ -210,17 +218,45 @@ function myFunction() {
             continue;
           }
         }
-        if (!found) {
-          pickList.push(itemObj);
-        }
 
-        // let pickListIndex = pickList.indexOf([item]);
-        // ( pickListIndex === -1)
-        // ? pickList.push([item])
-        // :pickList[pickListIndex];
+        if (params[i]["status"] == "processing") {
+          //if (itemObj.class ==
+
+          if (!found) {
+            pickList.push(itemObj);
+          }
+          if (true) {
+            let itemClass = "ex: dry goods";
+          }
+          let packRow = [
+            params[i]["billing"]["first_name"],
+            params[i]["billing"]["last_name"],
+            qty,
+            "",
+            item,
+            itemClass,
+            shippingAddress,
+            "",
+          ];
+          packList.push(packRow);
+          //on last iteration add quantity
+          if (k == c - 1) {
+            packRow = [
+              params[i]["billing"]["first_name"],
+              params[i]["billing"]["last_name"],
+              total_line_items_quantity,
+              "",
+              "ZZTotal",
+              "",
+              shippingAddress,
+              "",
+            ];
+            packList.push(packRow);
+          }
+        }
       }
-      // pickList.push({ass:'bites', tucus: 'mellow'})
-      Logger.log(pickList);
+      // customerRow = [params[i]["billing"]["email"], params[i]["billing"]["phone"], params[i]["billing"]["first_name"], params[i]["billing"]["last_name"], shippingAddress, '', '']
+      // customerList.push(customerRow);
 
       a = container.push(items);
 
@@ -269,7 +305,13 @@ function myFunction() {
       // var doc = SpreadsheetApp.getActiveSpreadsheet();
 
       // var temp = doc.getSheetByName(sheet_name);
-      ordersArray.push(container);
+      if (params[i]["status"] == "processing") {
+        ordersArray.push(container);
+      } else if (params[i]["status"] == "completed") {
+        ordersCompletedArray.push(container);
+      } else {
+        ordersOtherArray.push(container);
+      }
 
       // temp.appendRow(container);
 
@@ -278,9 +320,24 @@ function myFunction() {
       removeDuplicates(sheet_name);
     }
 
+    //ORDERS
+    //make this function to use 3 times
     var rows = ordersArray.length;
     var cols = ordersArray[0].length;
     temp.getRange(1, 1, rows, cols).setValues(ordersArray);
+
+    var completedRows = ordersCompletedArray.length;
+    var completedCols = ordersCompletedArray[0].length;
+    completedSheet
+      .getRange(1, 1, completedRows, completedCols)
+      .setValues(ordersCompletedArray);
+
+    var otherRows = ordersOtherArray.length;
+    var otherCols = ordersOtherArray[0].length;
+    otherSheet.getRange(1, 1, otherRows, otherCols).setValues(ordersOtherArray);
+
+    // //CUSTOMERS
+    //       customerHeaders = ['Email', 'Phone','First Name', 'Last Name', 'Address', 'Route', 'COMPLETED - Variation', 'Class', 'Address', 'Route'];
 
     //PICK
     pickList.sort(sortObjNames);
@@ -313,10 +370,10 @@ function myFunction() {
       .setFontSize("14");
     pickSheet.setColumnWidth(1, 400);
     pickSheet.setColumnWidth(3, 400);
-    pickSheet.getRange(1, 4, pickRows, 1).setHorizontalAlignment("center");
+    pickSheet.getRange(1, 3, pickRows, 1).setHorizontalAlignment("center");
     var pickLastColumn = pickSheet.getMaxColumns();
     var pickLastRow = pickSheet.getMaxRows();
-    pickSheet.deleteColumns(pickCols + 2, pickLastColumn - pickCols);
+    // pickSheet.deleteColumns(pickCols + 2, pickLastColumn - pickCols - 1);
     // pickSheet.deleteRows(pickRows + 1, pickLastRow - pickRows)
     pickSheet.getRange(1, 1, 1, pickCols).setFontWeight("bold");
     //Conditional Formatting
@@ -330,6 +387,85 @@ function myFunction() {
     pickRules.push(pickRule);
     pickSheet.setConditionalFormatRules(pickRules);
     //protect line item on pick, unprotect with UNDO button at top.
+
+    //PACK
+    let packArray = packList.sort(sort2DArr(1, 4));
+    Logger.log(packArray);
+    //Build packArray
+    // for (let item of packList) {
+    //   packArray.push([item.name, item.qty])
+    // }
+    //Add Headers
+    packHeaders = [
+      "First Name",
+      "Last Name",
+      "Quantity",
+      "Packed?",
+      "Product - Variation",
+      "Class",
+      "Address",
+      "Route",
+    ];
+    packArray.unshift(packHeaders);
+    Logger.log(packArray);
+    // Logger.log(packArray);
+    //getRange info
+    let packSheet = doc.getSheetByName("PACK");
+    var packRows = packArray.length;
+    var packCols = packArray[0].length;
+    //Set Values
+    packSheet.getRange(1, 1, packRows, packCols).setValues(packArray);
+    //Format
+    // packSheet.insertColumnAfter(3)
+    //highlight every other row for clarity
+    for (var i = 0; i < packRows; i++) {
+      if (i % 2 != 0) {
+        packSheet.getRange(i, 1, 1, packCols + 1).setBackgroundColor("#fffbf2");
+      }
+    }
+    packSheet
+      .getRange(2, 3, packRows, 2)
+      .setFontColor("black")
+      .setFontSize("14");
+    packSheet.setColumnWidth(3, 70);
+    packSheet.setColumnWidth(4, 50);
+    packSheet.setColumnWidth(5, 150);
+    packSheet.setColumnWidth(7, 200);
+    packSheet.getRange(2, 4, packRows - 1, 1).insertCheckboxes();
+    packSheet.getRange(1, 3, packRows, 1).setHorizontalAlignment("center");
+    packSheet.getDataRange().setVerticalAlignment("middle");
+    // var packHeaders = [["First Name", "Last Name", "Quantity", "", "Product", "Variation", "Address", "Shipping Method"]];
+    packSheet.getRange(1, 1, 1, packHeaders.length).setFontWeight("bold");
+    for (var i = 0; i < packRows; i++) {
+      if (packArray[i][4] == "ZZTotal") {
+        packSheet.getRange(i + 1, 3).setFontWeight("bold");
+        packSheet
+          .getRange(i + 1, 1, 1, packHeaders[0].length)
+          .setBackgroundColor("#9bb88e")
+          .setFontColor("#f3f3f3");
+      } else if (packArray[i][2] > 1) {
+        packSheet.getRange(i + 1, 3).setBackgroundColor("#e3f1df");
+      }
+    }
+    var packLastColumn = packSheet.getMaxColumns();
+    var packLastRow = packSheet.getMaxRows();
+    // packSheet.deleteColumns(packHeaders[0].length + 1, packLastColumn - packHeaders[0].length);
+    // packSheet.deleteRows(packRows + 1, packLastRow - packRows);
+    //Conditional Formatting
+    var packChecksRange = packSheet.getRange(
+      1,
+      1,
+      packRows,
+      packHeaders[0].length
+    );
+    var packRule = SpreadsheetApp.newConditionalFormatRule()
+      .whenFormulaSatisfied("=$D1")
+      .setBackground("#666666")
+      .setRanges([packChecksRange])
+      .build();
+    var packRules = packSheet.getConditionalFormatRules();
+    packRules.push(packRule);
+    packSheet.setConditionalFormatRules(packRules);
   }
 
   function removeDuplicates(sheet_name) {
@@ -396,10 +532,24 @@ function sortFunction(a, b) {
   }
 }
 
-function sortByProduct(a, b) {
-  if (a[0] === b[0]) {
-    return a[6] < b[6] ? -1 : a[6] > b[6] ? 1 : 0;
+function sort2DArray(a, b) {
+  if (a[1] === b[1]) {
+    return a[4] < b[4] ? -1 : a[4] > b[4] ? 1 : 0;
   } else {
-    return a[0] < b[0] ? -1 : 1;
+    return a[1] < b[1] ? -1 : 1;
   }
+}
+
+function sort2DArr(firstColToSort, secondColToSort) {
+  return function sort(a, b) {
+    if (a[firstColToSort] === b[firstColToSort]) {
+      return a[secondColToSort] < b[secondColToSort]
+        ? -1
+        : a[secondColToSort] > b[secondColToSort]
+        ? 1
+        : 0;
+    } else {
+      return a[firstColToSort] < b[firstColToSort] ? -1 : 1;
+    }
+  };
 }
